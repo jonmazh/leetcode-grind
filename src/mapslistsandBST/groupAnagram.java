46afd9c9c653806b259832456f5bdd3cd995fc75
Jon <jonmazh@gmail.com>
2026-03-10 13:19:38 -0300

    feat: 3 maps problems done

---
+++ src/mapslistsandBST/groupAnagram.java
@@ -0,0 +1,68 @@
+package mapslistsandBST;
+
+import java.util.*;
+
+public class groupAnagram {
+    /*
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    Example 1:
+
+    Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+    Explanation:
+
+    There is no string in strs that can be rearranged to form "bat".
+    The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+    The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+    Example 2:
+
+    Input: strs = [""]
+
+    Output: [[""]]
+
+    Example 3:
+
+    Input: strs = ["a"]
+
+    Output: [["a"]]
+
+
+
+    Constraints:
+
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lowercase English letters.
+     */
+
+    public static void main (String args[]){
+        LRUCache obj = new LRUCache(2);
+
+        obj.put(1, 1);
+        obj.put(2, 2);
+        obj.put(3, 3);
+        obj.get(2);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            char[] chars = (strs[i].toCharArray());
+            Arrays.sort(chars);
+            String aux = new String(chars);
+
+            if(!anagrams.containsKey(aux)){
+                List<String> values = new ArrayList<>();
+                values.add(strs[i]);
+                anagrams.put(aux, values);
+            } else {
+                List<String> value = anagrams.get(aux);
+                value.add(strs[i]);
+                anagrams.put(aux, value);
+            }
+        }
+        return new ArrayList<>(anagrams.values());
+    }
+}
---
+++ src/mapslistsandBST/topKfrequentElements.java
@@ -0,0 +1,59 @@
+package mapslistsandBST;
+
+import java.util.*;
+
+public class topKfrequentElements {
+    /*
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+    Example 1:
+
+    Input: nums = [1,1,1,2,2,3], k = 2
+
+    Output: [1,2]
+
+    Example 2:
+
+    Input: nums = [1], k = 1
+
+    Output: [1]
+
+    Example 3:
+
+    Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+    Output: [1,2]
+     */
+
+    public int[] topKFrequent(int[] nums, int k) {
+        // O(1) solution
+        if(k==nums.length){
+            return nums;
+        }
+
+        //O(N) solution -> First a map with (Number / Repetitions)
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
+        }
+
+        // Heap with least frequent first
+        Queue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> freq.get(n1) - freq.get(n2));
+
+        //Fill heap
+        for(int n : freq.keySet()){
+            heap.add(n);
+            if(heap.size()>k){
+                heap.poll();
+            }
+        }
+
+        //Convert from heap to solution
+        int[] sol = new int[k];
+        for(int i=0;i<k;i++){
+            sol[i] = heap.poll();
+        }
+
+        return sol;
+    }
+}
